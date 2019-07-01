dab89844d635e0d13a0288419873426b98300548
e-razzini <Elton@Pc-Casa>
2019-07-01 15:02:54 -0300

    comit inicial

--- src/imcanalize/ImcCalculos.java
+++ src/imcanalize/ImcCalculos.java
@@ -20,24 +20,6 @@ private Individuo ind;
     
    
       
-            if( resultado >= 18.5 &  resultado <=24.9)    
-            {
-             System.out.println("Peso normal.");
-            }else if( resultado >= 25 &  resultado <=29.9)
-            {
-              System.out.println("Sobrepeso.");
-            }else if(resultado >= 30 & resultado <=34.9)
-            {     
-             System.out.println("Obesidade grau um");
-             
-            }else if (resultado >=35 & resultado <=39.9)
-            {
-              System.out.println("Obesidade grau dois");
-            } else if (resultado >=40)       
-            {
-              System.out.println("Obesidade grau Três"); 
-            }
- 
-          return resultado;     
+            
     }
 }
