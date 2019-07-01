fc6de13769fbe8025a804655d049477fddc14c87
e-razzini <Elton@Pc-Casa>
2019-07-01 14:52:50 -0300

    comit inicial

--- src/situação/SituacaoPessoa.java
+++ src/situação/SituacaoPessoa.java
@@ -1,15 +1,8 @@
 
 package situação;
 
-import imcanalize.ImcCalculos;
-import modelo.Individuo;
-
 
 public class SituacaoPessoa {
-Individuo ind = new Individuo();
-    ImcCalculos ic = new ImcCalculos(ind);
-    
-    String situa;
-    double resultado = ic.calculo();
+
 
 }
