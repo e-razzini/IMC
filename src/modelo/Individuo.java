7d49bec8854928b59e95643f73233f0aaded303e
e-razzini <Elton@Pc-Casa>
2019-07-02 16:22:56 -0300

    alteração calculos 5

--- src/modelo/Individuo.java
+++ src/modelo/Individuo.java
@@ -5,6 +5,8 @@ public class Individuo {
 
 private double altura;
 private double peso;
+private Double resultado;
+private String situa;
 
     public double getAltura() {
         return altura;
@@ -20,5 +22,47 @@ private double peso;
 
     public void setPeso(double peso) {
         this.peso = peso;
+        this.setSitua();
     }
+
+    public Double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Double resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getSitua() {
+        return situa;
+    }
+
+    public void setSitua() {
+       if( resultado >= 18.5 &  resultado <=24.9){    
+
+situa ="Peso normal.";
+
+}else if( resultado >= 25 &  resultado <=29.9){
+
+situa ="Sobrepeso.";
+
+}else if(resultado >= 30 & resultado <=34.9){
+
+situa ="Obesidade grau um";
+
+}else if (resultado >=35 & resultado <=39.9){
+
+situa ="Obesidade grau dois";
+} else if (resultado >=40) {      
+
+situa ="Obesidade grau Três";
+
+}
+   
+        
+    }
+
+   
+
+    
 }
