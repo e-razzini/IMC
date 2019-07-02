f90de76c322615e1faddba2b0fe2529e8cd60a3c
e-razzini <Elton@Pc-Casa>
2019-07-02 16:21:22 -0300

    alteração calculos 5

--- src/situacao/SituacaoIndividuo.java
+++ src/situacao/SituacaoIndividuo.java
@@ -1,46 +1,14 @@
 
 package situacao;
 
-import imcanalize.ImcCalculos;
 import modelo.Individuo;
 
-
 public class SituacaoIndividuo {
-private Individuo ind;
- 
-    
-
+private Individuo ind; 
 
     public SituacaoIndividuo(Individuo ind) {
         this.ind = ind;
     }
- private String pessoa(){
-  ImcCalculos ic = new ImcCalculos(ind); 
-     
- double resultado = ic.calculo();     
- String situa =""; 
-  
-if( resultado >= 18.5 &  resultado <=24.9){    
-
-situa ="Peso normal.";
-
-}else if( resultado >= 25 &  resultado <=29.9){
-
-situa ="Sobrepeso.";
-
-}else if(resultado >= 30 & resultado <=34.9){
-
-situa ="Obesidade grau um";
-
-}else if (resultado >=35 & resultado <=39.9){
-
-situa ="Obesidade grau dois";
-} else if (resultado >=40) {      
-
-situa ="Obesidade grau Três";
-
 }
-    return situa;
- }
 
-}
+
